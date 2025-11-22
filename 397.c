018d1dc36408e0bec709260f5c0f7e6b5e747645
akashmalgunde <akashmalgunde56@gmail.com>
2025-11-22 09:00:36 +0530

    Add files via upload

---
+++ 397.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+void  Add(int *Arr,int *iCount,int iSize,int iNo)
+{
+    if(*iCount < iSize)
+    {
+        Arr[*iCount] = iNo;
+        (*iCount)++;
+    }
+    else{
+        printf("stack is full\n");
+    }
+}
+
+void Remove(int *Arr,int *iCount)
+{
+    Arr[(*iCount) - 1] = 0;
+    (*iCount)--;
+}
+
+void Display(int *Arr,int iCount)
+{
+    for(int i = 0; i < iCount; i++)
+    {
+        printf("%d ",Arr[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int iSize = 10;
+    int iCount = 3;
+    int Arr[10] = {10,20,30};
+
+    Display(Arr,iCount);
+
+
+    Add(Arr,&iCount,iSize,40);
+
+
+    Display(Arr,iCount);
+
+    Remove(Arr,&iCount);
+
+    Display(Arr,iCount);
+
+    Remove(Arr,&iCount);
+
+    Display(Arr,iCount);
+
+    return 0;
+}
